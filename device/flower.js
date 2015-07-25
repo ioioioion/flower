70f4a709ca7c9b8cdb36ce4e4432fa01f7f2f519
Nicky <xi.nicky@gmail.com>
2015-07-25 14:25:28 +0800

    change i2c

--- device/flower.js
+++ device/flower.js
@@ -17,20 +17,19 @@
 
 exports.pins = {
 	wetSensor: { type: "A2D" },
-	i2cSensors: {type: "I2C", address: 0x48},
+	i2cTemp: {type: "I2C", address: 0x48},
+    i2cLumin: {type: "I2C", address: 0x29},
     watering: {type: "Digital", direction: "output"},
     led: {type: "Digital", direction: "output"},
     
 };
 
-exports.configure = function() {
+exports.configure = function () {
     this.wetSensor.init();
-     if ( "i2cSensors" in this ) {
-    this.i2cSensors.init();
-    this.previous = -1;
-    }
-     this.led.init();
-     this.watering.init();
+    this.i2cTemp.init();
+    this.i2cLumin.init();
+    this.led.init();
+    this.watering.init();
 }
 
 exports.wetread = function() {
@@ -38,11 +37,11 @@ exports.wetread = function() {
 }
 
 exports.lightread = function() {
-    return this.i2cSensors.read();
+    return this.i2cLumin.read();
 }
 
 exports.tempread = function () {
-    var data = this.i2cSensors.readWordDataSMB(0);
+    var data = this.i2cTemp.readWordDataSMB(0);
 	var value = ((data & 0xFF) << 4) | ((data >> 8) >> 4);
 	if (value & 0x800) {
 	    value -= 1;
@@ -61,6 +60,7 @@ exports.tempread = function () {
 exports.close = function() {
 	this.wetSensor.close();
 	this.i2cSensors.close();
-    this.led.close();
+    this.i2cTemp.close();
+    this.i2cLumin.close();
     this.watering.close();
 }
